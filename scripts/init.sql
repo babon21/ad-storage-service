f40ebdaa943e907e7534c2ba31eaffecc840df2e
Darzhain Dmitriy <kelan007@yandex.ru>
2021-02-18 03:21:56 +0300

    added postgres index

--- scripts/init.sql
+++ scripts/init.sql
@@ -14,3 +14,8 @@ CREATE TABLE ad
     photos      TEXT[] NOT NULL,
     date_added  TIMESTAMP     NOT NULL
 );
+
+CREATE INDEX ad_price_index ON ad (price);
+CREATE INDEX ad_price_desc_index ON ad (price DESC);
+CREATE INDEX ad_date_added_index ON ad (date_added);
+CREATE INDEX ad_date_added_desc_index ON ad (date_added DESC);
